9e5d6614ed646d368f352c5634e29234b2264271
07naoki07 <naokiti.0205.22@gmail.com>
2022-04-06 07:12:44 +0000

    record update

--- app/controllers/user/records_controller.rb
+++ app/controllers/user/records_controller.rb
@@ -1,2 +1,41 @@
 class User::RecordsController < ApplicationController
+  
+  def new
+   @record = Record.new
+  end
+  
+  def index
+   @records =Record.all
+  end
+  
+  def create
+   @record = Record.new(record_params)
+   @record.save
+  end
+  
+  def show
+   @record = Record.find(params[:id])
+  end
+  
+  def edit
+   @record = Record.find(params[:id])
+  end
+  
+  def update
+   @record = Record.find(params[:id])
+   @record.update(record_params)
+   redirect_to record_path(@record.id)
+  end
+  
+  def destroy
+    @record = Record.find(params[:id])
+    @record.destroy
+    redirect_to records_path
+  end
+  
+  private
+  
+  def record_params
+   params.require(:record).permit(:name, :shop_name, :review, :rate, :address, :image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,34 @@
 Rails.application.routes.draw do
 
-  devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-  }
-  devise_for :users, controllers: {
-  sessions:      'users/sessions',
-  passwords:     'users/passwords',
-  registrations: 'users/registrations'
-  }
-
   get 'homes/top'
   root to:"homes#top"
+
+　namespace :admin do
+　 resources :records, only:[:index, :show, :destroy]
+　 resources :comments, only:[:show, :destroy]
+　end
+
+　scope module: :user do
+   resources :records
+   resources :likes, only:[:create, :deestroy]
+   resources :comments, only:[:create, :destroy]
+   resources :searches, only:[:search]
+  end
+ end
+
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:passwords] ,controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
