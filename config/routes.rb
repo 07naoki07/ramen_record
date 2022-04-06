51e82243d1fb1a8b1389b8abb3397aceba09a11e
07naoki07 <naokiti.0205.22@gmail.com>
2022-04-06 17:14:04 +0000

    updaatte

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,30 @@
 Rails.application.routes.draw do
+　root to:"homes#top"
 
-  get 'homes/top'
-  root to:"homes#top"
-
-　namespace :admin do
-　 resources :records, only:[:index, :show, :destroy]
-　 resources :comments, only:[:show, :destroy]
-　end
-
-　scope module: :user do
-   resources :records
-   resources :likes, only:[:create, :deestroy]
-   resources :comments, only:[:create, :destroy]
-   resources :searches, only:[:search]
-  end
- end
-
-  # 顧客用
-  # URL /customers/sign_in ...
+  # 会員用
+  # URL /users/sign_in ...
   devise_for :users,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
   }
 
   # 管理者用
   # URL /admin/sign_in ...
-  devise_for :admin, skip: [:passwords] ,controllers: {
-    registrations: "admin/registrations",
-    sessions: "admin/sessions"
+  devise_for :admins, skip: [:passwords] ,controllers: {
+    registrations: "admins/registrations",
+    sessions: "admins/sessions"
   }
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+　 resources :records, only:[:index, :show, :destroy]
+　 resources :comments, only:[:show, :destroy]
+　end
+
+  namespace :user do
+   resources :records, only:[:new, :create, :index, :show, :edit, :update, :destroy]
+   resources :likes, only:[:create, :deestroy]
+   resources :comments, only:[:create, :destroy]
+   resources :searches, only:[:search]
+  end
+end
 end
