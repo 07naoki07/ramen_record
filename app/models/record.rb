28c58029208d450f8f73d8bdfac0c64919ea7a0b
07naoki07 <naokiti.0205.22@gmail.com>
2022-04-08 04:42:39 +0000

    view update

--- app/models/record.rb
+++ app/models/record.rb
@@ -6,4 +6,12 @@ class Record < ApplicationRecord
  has_many :likes, dependent: :destroy
  has_many :comments, dependent: :destroy
  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+ 
 end
