68584d83c8b9a03eac34bd1599c98f05900dbde7
07naoki07 <naokiti.0205.22@gmail.com>
2022-04-13 18:31:50 +0000

    update

--- app/controllers/user/ramen_shops_controller.rb
+++ app/controllers/user/ramen_shops_controller.rb
@@ -1,3 +1,42 @@
 class User::RamenShopsController < ApplicationController
   
+  def index
+   @ramen_shop = Ramen_Shop.new
+   @ramen_shops = Ramen_Shop.all
+  end
+  
+  def create
+   @ramen_shop = Ramen_Shop.new(ramen_shop_params)
+   if @ramen_shop.save
+     redirect_to ramen_shops_path
+   else
+     render :index
+   end
+  end
+  
+  def show
+   @ramen_shop = Ramen_Shop.find(params[:id])
+  end
+  
+  def edit
+    @ramen_shop = Ramen_Shop.find(params[:id])
+  end
+  
+  def update
+    @record = Record.find(params[:id])
+    ramen_shop_list = params[:record][:ramen_shop].split(',')
+    if @record.update(record_paramas)
+       @record.save_ramen_shops(ramen_shop_list)
+       redirect_to record_path(@record.id)
+    else
+      render index
+    end
+  end
+  
+  private
+  
+  def ramen_shop_params
+    params.require(:record).permit(:user_id, :name, :shop_name, :caption, :rate, :address, :image)
+  end
+  
 end
--- app/models/ramen_shop.rb
+++ app/models/ramen_shop.rb
@@ -1,2 +1,3 @@
 class RamenShop < ApplicationRecord
+  
 end
