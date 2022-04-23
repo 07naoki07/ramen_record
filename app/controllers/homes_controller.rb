07dcc9f76f1056e7eb12fe6678774b920a50ca39
07naoki07 <naokiti.0205.22@gmail.com>
2022-04-23 04:36:08 +0000

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def top
    @records = Record.all.order("created_at DESC").limit(3)
+   @all_ranks = Record.find(Like.group(:record_id).order('count(record_id) desc').limit(3).pluck(:record_id))
   end
 end
