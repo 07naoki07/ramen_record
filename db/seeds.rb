135e3673ca880592e7a57bd559103ad8004ccac7
07naoki07 <naokiti.0205.22@gmail.com>
2022-04-27 12:39:15 +0000

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+[
+ {email: 'test@test.com', name: '直樹', password: 'password'},
+ {email: 'test1@test.com', name: '山田花子', password: 'password'},
+ {email: 'test2@test.com', name: '大阪太郎', password: 'password'},
+ {email: 'test3@test.com', name: 'ラーメンマン', password: 'password'},
+ {email: 'test4@test.com', name: 'ラーメン大好き', password: 'password'},
+ {email: 'test5@test.com', name: '週７ラーメン', password: 'password'},
+
+]
+)
