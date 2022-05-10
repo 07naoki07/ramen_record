e39673ddcda7efcbd49d7efb98525d8890c5f81b
07naoki07 <naokiti.0205.22@gmail.com>
2022-05-10 17:11:51 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2022_04_13_140705) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
